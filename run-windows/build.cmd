9f0827b9bbc5c59679ac53c94ca6c0f5db2332b8
DevNielson <212224575+DevNielson@users.noreply.github.com>
2026-07-29 17:57:45 -0300

    build.cmd configured

--- run-windows/build.cmd
+++ run-windows/build.cmd
@@ -6,10 +6,16 @@ set MSYS2=%errorlevel%
 winget list --name "LLVM MinGW" >nul 2>&1
 set MinGW=%errorlevel%
 
-if %MSYS2% equ 0 (
-    cmake -G "MSYS Makefiles" -S .. -B ../build
-) else if %MinGW% equ 0 (
-    cmake -G "MinGW Makefiles" -S .. -B ../build
+if %MSYS2% EQU 0 (
+    cmake -G "MSYS Makefiles" -S .. -B ..\build
+) else if %MinGW% EQU 0 (
+    cmake -G "MinGW Makefiles" -S .. -B ..\build
 ) else (
     echo C++ compiler not found!
+)
+
+dir ..\build\SDL3.dll
+
+if %errorlevel% NEQ 0 (
+    copy ..\external\SDL3\x86_64-w64-mingw32\bin\SDL3.dll ..\build
 )
